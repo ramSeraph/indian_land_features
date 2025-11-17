a0e00ff428951584a5bbb54eb1628ce4bfc69528
ramSeraph <kandimalla.sreeram@gmail.com>
2025-11-17 14:04:18 +0530

    forgot to add the clipping

--- soi/steps.sh
+++ soi/steps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uvx --from wmsdump wms-extractor extract -u https://uat.soiindia.in/geoserver/soi/wms soi:CONTOUR_Crv -s wms -b 1000 -m extent --bounds "67.38,7.97,97.37,35.37" --max-box-dims "0.1,0.1"
+uvx --from wmsdump wms-extractor extract -u https://uat.soiindia.in/geoserver/soi/wms soi:CONTOUR_Crv -s wms -b 1000 -m extent --bounds "67.38,7.97,97.37,35.37" --max-box-dims "0.1,0.1" --geometry-precision 7
 
 uvx --from wmsdump geojsonl-dedupe soi_CONTOUR_Crv.geojsonl
 
